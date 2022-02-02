000dd342678d719ea2066132dce4bce39175e176
SoraY677 <SoraY677@outlook.com>
2022-02-02 21:38:27 +0900

    試し②

--- potatis/PotatisSourceSelect/script.js
+++ potatis/PotatisSourceSelect/script.js
@@ -32,32 +32,32 @@ const confirmSource = (index)=>{
 		if (selectedSourceIndex != -1)
 			sourceSelecterContainerList[selectedSourceIndex].classList.remove('selected')
 		selectedRect = sourceSelecterContainerList[index].getBoundingClientRect()
-		sourceSelecterContainerList[index].animate(
-			[
-				{width:`${selectedRect.width}px`,transform:'translate(0,0)', margin:`0`},
-				{width:'40%',transform:`translate(${-1 * selectedRect.x}px,${-1 * selectedRect.y}px)`,margin:`30px 30% 0 30%`}
-
-			],{
-			duration: 600,
-			easing: 'linear',
-			fill: 'forwards',
-			iterations: 1
-			}
-		)
-		
-		// ソース説明のアニメーション
-		// sourceDetail.style.top = `${parseInt(selectedRect.y + + selectedRect.height / 2)}px`
-		// sourceDetail.style.left = `${parseInt(selectedRect.x + selectedRect.width / 2)}px`
-		// sourceDetail.animate(
+		// sourceSelecterContainerList[index].animate(
 		// 	[
-		// 		{width:`${0}px`, height:`${0}px`, margin:`0`, transform:'translate(0,0)'},
-		// 		{width:`${80}%`,height:`${50}%`, margin:`140px 10% 0 10%`, transform:`translate(${-1 * (selectedRect.x + + selectedRect.width / 2)}px,${-1 * (selectedRect.y + + selectedRect.height / 2)}px)`},
+		// 		{width:`${selectedRect.width}px`,transform:'translate(0,0)', margin:`0`},
+		// 		{width:'40%',transform:`translate(${-1 * selectedRect.x}px,${-1 * selectedRect.y}px)`,margin:`30px 30% 0 30%`}
+
 		// 	],{
 		// 	duration: 600,
 		// 	easing: 'linear',
-		// 	fill: 'forwards'
+		// 	fill: 'forwards',
+		// 	iterations: 1
 		// 	}
 		// )
+		
+		// ソース説明のアニメーション
+		sourceDetail.style.top = `${parseInt(selectedRect.y + + selectedRect.height / 2)}px`
+		sourceDetail.style.left = `${parseInt(selectedRect.x + selectedRect.width / 2)}px`
+		sourceDetail.animate(
+			[
+				{width:`${0}px`, height:`${0}px`, margin:`0`, transform:'translate(0,0)'},
+				{width:`${80}%`,height:`${50}%`, margin:`140px 10% 0 10%`, transform:`translate(${-1 * (selectedRect.x + + selectedRect.width / 2)}px,${-1 * (selectedRect.y + + selectedRect.height / 2)}px)`},
+			],{
+			duration: 600,
+			easing: 'linear',
+			fill: 'forwards'
+			}
+		)
 	
 		overlay.classList.add('visible')
 		sourceSelecterContainerList[index].classList.add('selected')
@@ -73,29 +73,29 @@ const confirmSource = (index)=>{
 overlay.onclick = () => {
 	overlay.classList.remove('visible')
 
-	sourceSelecterContainerList[selectedSourceIndex].animate(
-		[
-			{width:'40%',transform:`translate(${-1 * selectedRect.x}px,${-1 * selectedRect.y}px)`,margin:`30px 30% 0 30%`},
-			{width:`${selectedRect.width}px`,transform:'translate(0,0)', margin:`0`}
-
-		],{
-		duration: 600,
-    	easing: 'linear',
-    	fill: 'forwards',
-		iterations: 1
-		}
-	)
-
-	// sourceDetail.animate(
+	// sourceSelecterContainerList[selectedSourceIndex].animate(
 	// 	[
-	// 		{width:`${80}%`,height:`${50}%`, margin:`140px 10% 0 10%`, transform:`translate(${-1 * (selectedRect.x + + selectedRect.width / 2)}px,${-1 * (selectedRect.y + + selectedRect.height / 2)}px)`},
-	// 		{width:`${0}px`, height:`${0}px`, margin:`0`, transform:'translate(0,0)'},
+	// 		{width:'40%',transform:`translate(${-1 * selectedRect.x}px,${-1 * selectedRect.y}px)`,margin:`30px 30% 0 30%`},
+	// 		{width:`${selectedRect.width}px`,transform:'translate(0,0)', margin:`0`}
+
 	// 	],{
 	// 	duration: 600,
     // 	easing: 'linear',
-    // 	fill: 'forwards'
+    // 	fill: 'forwards',
+	// 	iterations: 1
 	// 	}
 	// )
+
+	sourceDetail.animate(
+		[
+			{width:`${80}%`,height:`${50}%`, margin:`140px 10% 0 10%`, transform:`translate(${-1 * (selectedRect.x + + selectedRect.width / 2)}px,${-1 * (selectedRect.y + + selectedRect.height / 2)}px)`},
+			{width:`${0}px`, height:`${0}px`, margin:`0`, transform:'translate(0,0)'},
+		],{
+		duration: 600,
+    	easing: 'linear',
+    	fill: 'forwards'
+		}
+	)
 	isSelectedMode = false
 
 }
@@ -113,31 +113,31 @@ for(let i = 0; i < sourceSelecterContainerList.length; i++){
  * ソースが決められたとき
  */
 sourceDecideBtn.onclick = () => {
-	sourceSelecterContainerList[selectedSourceIndex].animate(
-		[
-			{width:'40%',transform:`translate(${-1 * selectedRect.x}px,${-1 * selectedRect.y}px)`,margin:`30px 30% 0 30%`},
-			{width:'22%',transform:`translate(${-1 * selectedRect.x}px,${window.innerHeight - selectedRect.y - selectedRect.height}px)`,margin:`0 0 5% 5%`},
-			
-		],{
-		duration: 1000,
-    	easing: 'linear',
-    	fill: 'forwards',
-		iterations: 1
-		}
-	)
-	
-	// selectedRect = sourceSelecterContainerList[selectedSourceIndex].getBoundingClientRect()
-	// sourceDetail.animate(
+	// sourceSelecterContainerList[selectedSourceIndex].animate(
 	// 	[
-	// 		{width:`${80}%`,height:`${50}%`, margin:`140px 10% 0 10%`, transform:`translate(${-1 * (selectedRect.x + selectedRect.width / 2)}px,${-1 * (selectedRect.y + selectedRect.height / 2)}px)`},
-	// 		{width:`${0}px`, height:`${0}px`, margin:`140px 50% 0 50%`, transform:`translate(${-1 * (selectedRect.x + selectedRect.width / 2)}px,${-1 * (selectedRect.y + selectedRect.height)}px)`},
+	// 		{width:'40%',transform:`translate(${-1 * selectedRect.x}px,${-1 * selectedRect.y}px)`,margin:`30px 30% 0 30%`},
+	// 		{width:'22%',transform:`translate(${-1 * selectedRect.x}px,${window.innerHeight - selectedRect.y - selectedRect.height}px)`,margin:`0 0 5% 5%`},
+			
 	// 	],{
-	// 	duration: 300,
+	// 	duration: 1000,
     // 	easing: 'linear',
-    // 	fill: 'forwards'
+    // 	fill: 'forwards',
+	// 	iterations: 1
 	// 	}
-		
 	// )
+	
+	selectedRect = sourceSelecterContainerList[selectedSourceIndex].getBoundingClientRect()
+	sourceDetail.animate(
+		[
+			{width:`${80}%`,height:`${50}%`, margin:`140px 10% 0 10%`, transform:`translate(${-1 * (selectedRect.x + selectedRect.width / 2)}px,${-1 * (selectedRect.y + selectedRect.height / 2)}px)`},
+			{width:`${0}px`, height:`${0}px`, margin:`140px 50% 0 50%`, transform:`translate(${-1 * (selectedRect.x + selectedRect.width / 2)}px,${-1 * (selectedRect.y + selectedRect.height)}px)`},
+		],{
+		duration: 300,
+    	easing: 'linear',
+    	fill: 'forwards'
+		}
+		
+	)
 
 	overlay.classList.remove('visible')
 }
