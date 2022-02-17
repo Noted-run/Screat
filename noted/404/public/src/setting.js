790dab4548454728a0feb09b7e96713af462f153
SoraY677 <SoraY677@outlook.com>
2022-02-17 21:52:36 +0900

    SVGの描画まで

--- noted/404/public/src/setting.js
+++ noted/404/public/src/setting.js
@@ -1,12 +1,22 @@
 import Matter from "matter-js"
+// svgレンダリングに必要１
+import * as pathseg from "./plugins/pathseg" 
 
+const Common = Matter.Common
 const Engine = Matter.Engine
 const Render = Matter.Render
 const Runner = Matter.Runner
 const Bodies = Matter.Bodies
 const Composite = Matter.Composite
+const Vertices = Matter.Vertices
+const Svg = Matter.Svg
+const Mouse = Matter.Mouse
+const MouseConstraint = Matter.MouseConstraint
+// SVGをレンダリングするのに必要２
+Common.setDecomp(require('poly-decomp'));
 
 const engine = Engine.create()
+const world = engine.world;
 
 const render = Render.create({
     element: document.body,
@@ -39,11 +49,51 @@ const render = Render.create({
     }
 });
 
+const mouse = Mouse.create(render.canvas)
+const mouseConstraint = MouseConstraint.create(engine, {
+	mouse: mouse,
+	constraint: {
+		stiffness: 0.2,
+		render: {
+			visible: false
+		}
+	}
+});
+Composite.add(world, mouseConstraint);
+render.mouse = mouse;
+
+var select = function(root, selector) {
+    return Array.prototype.slice.call(root.querySelectorAll(selector));
+};
+var loadSvg = function(url) {
+    return fetch(url)
+        .then(function(response) { return response.text(); })
+        .then(function(raw) { return (new window.DOMParser()).parseFromString(raw, 'image/svg+xml'); });
+};
+
+loadSvg('./resource/crush.svg').then(function(root) {
+    var color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
+    
+    var vertexSets = select(root, 'path')
+        .map(function(path) { 
+            return Svg.pathToVertices(path); 
+        });
+    
+    Composite.add(world, Bodies.fromVertices(400, 80, vertexSets, {
+        render: {
+            fillStyle: color,
+            strokeStyle: color,
+            lineWidth: 1
+        }
+    }, true));
+});
 
 
-function addComposite(...bodies){
-	Composite.add(engine.world, bodies)
-}
+// function addComposite(...bodies){
+// 	Composite.add(world, bodies)
+// }
+
+
 
 function runRender(){
 	Render.run(render)
@@ -58,7 +108,8 @@ export {
 	Runner,
 	Bodies,
 	Composite,
-	addComposite,
+	Mouse,
+	// addComposite,
 	runRender}
 
 
