171ce15e85f43caa2377bb6bbae41fb17a8ffdcc
SoraY677 <SoraY677@outlook.com>
2022-02-18 23:25:24 +0900

    path指定を変更

--- src/noted/404/public/src/main.js
+++ src/noted/404/public/src/main.js
@@ -1,9 +1,10 @@
+import svg404Path from '~/resource/crush.svg';
 import {runRender}  from "./setting"
 import { create404Obj } from "./stage"
 (function(){
 	window.onload = () => {
 		// main
-		create404Obj('resource/crush.svg')
+		create404Obj(svg404Path)
 		
 		runRender()
 		
